db938e4ea870b52d10e29e56384936ba75f455c4
dk-17 <kambariadivya212@gmail.com>
2021-11-14 18:01:37 +0530

    added signin functionality just by render plain text(without session)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,21 @@ class HomeController < ApplicationController
   def index
    render "index"
   end
+
+  def new
+    # by default it willrender new.htm.erb file
+  end
+
+  def create
+    falg = true
+
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+          render plain: "You have entered the correct password!"
+     else
+        render plain: "Incorrect password!"
+     end
+
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/" => "home#index"
 
+  post "/" => "home#create", as: :sessions
   resources :users
 
 end
